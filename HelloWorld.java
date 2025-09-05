c6aca8a562ad0e49240a09ae9d270320534eb1bd
YHS <yshin5@albany.edu>
2025-09-05 14:07:36 -0400

    HelloWorld added

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // This is the main method, the entry point of the program.
+        System.out.println("Hello, World!");
+    }
+}
