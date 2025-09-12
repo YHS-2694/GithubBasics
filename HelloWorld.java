9aee3ef8246a7684c6b4287aee1f2279c2a6d578
PRANAW2004 <pranawrk2@gmail.com>
2025-09-12 13:03:50 -0400

    Edit program

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // This is the main method, the entry point of the program.
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World");
     }
 }
